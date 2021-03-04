46f9835d7073c88c880053e120fb7be5e0bb669f
rsmith <legendary.belief@gmail.com>
2021-03-03 20:13:22 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+	answr = true
+	if number > 2
+		for i in (2..(number - 1)).to_a
+			return answr = false if number % i == 0
+		end
+	elsif number < 2
+		answr = false
+	end
+	answr
+end
